98519c583d53dc1e8f93469e57659cd87fed636b
s0919202530 <s0919202530@gmail.com>
2012-07-20 15:35:46 +0800

    add isPayed to help us to know who has payed

--- list.php
+++ list.php
@@ -4,13 +4,38 @@
 // 建立 session
 if (!isset($_SESSION)) {
   session_start();
-
+}
 
 // 前一個網頁
 $_SESSION['PrevPage'] = "index.php";
 
 $groupId=$_GET['groupId'];
 if(!$groupId) {header(sprintf("Location: %s", $_SESSION['PrevPage']));}
+?>
+
+<?php
+//**********************************//
+// 在orderLog資料表內插入一筆新的紀錄
+//**********************************//
+if (($_POST["insert"] == "49821032")) 
+{
+	// 選擇 MySQL 資料庫lunch
+	mysql_select_db('lunch', $connection) or die('資料庫lunch不存在');
+	
+	for($i=0;$i<count($isPayed);$i++)
+	{
+		$temp=$isPayed[$i];
+		// 在orderLog資料表內插入一筆新的紀錄
+		$query = "UPDATE orderLog SET isPayed = '1' WHERE id = '$temp' ";
+			
+		// 傳回結果集
+		$result = mysql_query($query, $connection) or die(mysql_error());
+	}
+	
+	if ($result) {
+	    // 回到前一個網頁 
+	  	//header(sprintf("Location: %s", $_SESSION['PrevPage']));
+	}
 }
 ?>
 
@@ -26,6 +51,7 @@ if(!$groupId) {header(sprintf("Location: %s", $_SESSION['PrevPage']));}
 			  border: 1px solid #000000;  
 			  border-collapse: collapse;  
 			  text-align:center;
+			  margin-top:100px;
 			}  
 			tr, td {  
 			  border: 1px solid #000000;  
@@ -36,16 +62,15 @@ if(!$groupId) {header(sprintf("Location: %s", $_SESSION['PrevPage']));}
 	
 	
 	<body>
-		<!--- <table style="position:relative; margin-left:auto; margin-right:auto; width:40%; ">
+	<form method="post" action="<?php htmlentities($_SERVER['PHP_SELF']); ?>">
+		<table style="position:relative; margin-left:auto; margin-right:auto; width:40%; ">
+		
 			<tr>
 				<td>
 					訂單
 				</td>
 				<td>
 					付了嗎?
-				</td>
-				<td>
-					領了嗎?
 				</td>
 			</tr>
 			<?php
@@ -61,27 +86,43 @@ if(!$groupId) {header(sprintf("Location: %s", $_SESSION['PrevPage']));}
 						<tr>
 							<td bgColor="#E6E6FA">
 								<?php 
+									$temp1=$row1['foodName'];
 									echo $row1['userName'] . "&nbsp;" . $row1['foodCount'] . "個&nbsp;" . $row1['foodName'];
 									if($row1['userRemark']=="加飯/麵") { echo "&nbsp;加飯/麵"; }
 									if($row1['userRemark']=="減飯/麵") { echo "&nbsp;減飯/麵"; }
 									if($row1['userRemark']=="去冰") { echo "&nbsp;去冰"; }
 									if($row1['userRemark']=="少冰") { echo "&nbsp;少冰"; }
 								?>
-							</td>
-							<td>
-								<input type="checkbox" id="pay" name="pay[]" onclick="pay.disabled=!pay.disabled">
-							</td>
-							<td>
-								<input type="checkbox" id="take" name="take[]" onclick="take.disabled=!take.disabled">
+								<?php 
+									$query2 = "SELECT shopName FROM lunchGroup WHERE id = '$groupId' ";
+									// 傳回結果集
+									$result2 = mysql_query($query2, $connection) or die(mysql_error());
+									if ($result2) { $row2 = mysql_fetch_assoc($result2); }
+									$shopName=$row2['shopName'];
+								
+									$query3 = "SELECT foodPrice FROM menu WHERE shopName = '$shopName' AND foodName = '$temp1' ";
+									// 傳回結果集
+									$result3 = mysql_query($query3, $connection) or die(mysql_error());
+									if ($result3) { $row3 = mysql_fetch_assoc($result3); }
+								?>
 							</td>
 							<?php
-								if($userlevel==1) {
+								if($row1['isPayed']==0) {
 							?>
-								<td>
-									<input type="button" value="X" onclick="self.location.href='delete.php?groupId=<?php echo $row1['id'] ?>&userlevel=1'">
-								</td>
+							<td>
+								<input type="checkbox" id="isPayed" name="isPayed[]" value="<?php echo $row1['id']; ?>"><?php echo $row1['foodCount']*$row3['foodPrice']."元"; ?>
+							</td>
 							<?php
-								};
+								}
+							?>
+							<?php
+								if($row1['isPayed']!=0) {
+							?>
+							<td>
+								<input type="checkbox" id="isPayed" name="isPayed[]" checked disabled><?php echo $row1['foodCount']*$row3['foodPrice']."元"; ?>
+							</td>
+							<?php
+								}
 							?>
 						</tr>
 			<?php
@@ -89,8 +130,16 @@ if(!$groupId) {header(sprintf("Location: %s", $_SESSION['PrevPage']));}
 				};
 			}
 			?>
-		</table> --->
-		功能尚未開放。<br/>
-		<a href="index.php">回到上一頁</a>
+
+		</table>
+
+		
+		<div align="center" style="margin-top:10px; margin-right:100px;">
+			主揪專屬密碼：<input name="insert" id="insert" type="password" />
+			<input id="send" name="submit" type="submit" value="送出" onclick="return CheckFields();" />
+		</div><br/>
+
+	</form>
+		<center><a href="index.php">回到上一頁</a></center>
 	</body>
 </html>
